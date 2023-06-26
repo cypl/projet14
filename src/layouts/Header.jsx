fbbc35c200e569068db3ccd817c563e88469448c
cypl <cyrille.plenchette@me.com>
2023-06-26 22:24:19 +0200

    add Header

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,13 +1,54 @@
+import styled from 'styled-components'
+import { NavLink } from 'react-router-dom'
+
 function Header(){
     return(
         <>
-            <header>
-                <div className="content_width">
-                    <p>HRNET</p>
-                </div>
-            </header>
+            <HeaderContainer>
+                <HeaderContent className="content_width">
+                    <Branding><NavLink to="/">HRNET</NavLink></Branding>
+                    <Menu>
+                        <ul>
+                            <li><MenuLink to="/">Current employees</MenuLink></li>
+                            <li><MenuLink to="/create">Create</MenuLink></li>
+                        </ul>
+                    </Menu>
+                </HeaderContent>
+            </HeaderContainer>
         </>
     )
 }
 
 export default Header
+
+const HeaderContainer = styled.header`
+    border-bottom:1px solid rgba(0,0,0,0.1);
+`
+const Branding = styled.h1`
+    font-size:24px;
+    line-height:33px;
+    font-weight:900;
+    letter-spacing:-1px;
+`
+const HeaderContent = styled.div`
+    padding:40px 0;
+    display:flex;
+    justify-content:space-between;
+`
+const Menu = styled.nav`
+    & ul li{
+        display:inline-block;
+        line-height:33px;
+    }
+`
+const MenuLink = styled(NavLink)`
+    font-size:13px;
+    text-transform:uppercase;
+    padding:10px 15px;
+    margin-left:15px;
+    background-color:rgba(0,0,0,0.1);
+    border-radius:4px;
+    &.active{
+        background-color:rgba(0,0,0,0.25);
+    }
+`
