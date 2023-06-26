0ea00e1763de6e57582833bb6c66820ee068dd9a
cypl <cyrille.plenchette@me.com>
2023-06-26 22:33:44 +0200

    add Footer

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,7 +1,22 @@
+import styled from 'styled-components'
+
+const currentYear = new Date().getFullYear()
+
 function Footer(){
     return(
-        <p>Footer</p>
+        <FooterContainer>
+            <FooterContent className="content_width">
+                <p className="legende">©{currentYear} HRNET - Wealth Health</p>
+            </FooterContent>
+        </FooterContainer>
     )
 }
 
 export default Footer
+
+const FooterContainer = styled.footer`
+    border-top:1px solid rgba(0,0,0,0.1);
+`
+const FooterContent = styled.div`
+    padding:20px 0;
+`
