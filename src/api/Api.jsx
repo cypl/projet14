5fc1489bdde04c04d497c52b6d5780b04d9dcc03
cypl <cyrille.plenchette@me.com>
2023-06-27 10:49:35 +0200

    add API and fetch current employees

---
+++ src/api/Api.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react"
+import axios from 'axios'
+
+
+const useFetchData = (path) => {
+    const [data, setData] = useState({})
+    const [isLoaded, setLoaded] = useState(false)
+    const [isError, setError] = useState()
+
+    useEffect(() => {
+        async function FetchData(path) {
+            setLoaded(false)
+            try {
+                const response = await axios.get(path)
+                setData(response.data)
+                setError(null)
+                setLoaded(true)
+            } catch (error) {
+                setError(error)
+                setLoaded(true)
+            }
+        }
+        FetchData(path)
+    }, [path])
+
+    return {data, isLoaded, isError}
+
+}
+
+
+export const GetDataEmployees = () => { return useFetchData("public/data/mocked-employees.json") }
--- src/pages/CurrentEmployees.jsx
+++ src/pages/CurrentEmployees.jsx
@@ -1,8 +1,18 @@
+import { GetDataEmployees } from "../api/Api"
+
 function CurrentEmployees(){
+
+    const employees = GetDataEmployees()
+
     return(
         <main>
             <section className="content_width">
                 <p>Here, you can see all current employees.</p>
+                {employees.isLoaded && employees.isError === null &&
+                    employees.data.map((e, index) => (
+                        <p key={index}>{e.firstName}</p>
+                    ))
+                }
             </section>
         </main>
     )
