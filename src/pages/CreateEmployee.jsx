696efb2d8386445057ce2d4cac82bd57356b9ae7
cypl <cyrille.plenchette@me.com>
2023-06-28 20:12:52 +0200

    add random data

--- src/pages/CreateEmployee.jsx
+++ src/pages/CreateEmployee.jsx
@@ -8,6 +8,12 @@ import { departmentsList } from "../utils/departmentsList"
 
 
 const statesNames = statesList.map((s) => s.name)
+console.log(statesNames)
+function getRandomValue(array) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+}
+
 
 function formatEmployeeData(data){
     return {
@@ -19,7 +25,7 @@ function formatEmployeeData(data){
         street: data.street ?? "",
         city: data.city ?? "",
         state: data.state ?? "",
-        zipCode: data.zipCode ?? "",
+        zipCode: "" + data.zipCode ?? "",
     }
 }
 function formatDateString(dateObject){
@@ -60,32 +66,51 @@ function CreateEmployee(){
         console.log(formatEmployeeData(newEmployee))
     }
 
-    function fillTheForm(){
-        const birth = new Date()
-        birth.setFullYear(1983)
-        birth.setMonth(6)
-        birth.setDate(27)
-        const start = new Date()
-        start.setFullYear(2017)
-        start.setMonth(11)
-        start.setDate(1)
-
-        setFirstName("John")
-        setLastName("Smith")
-        setDateOfBirth(birth)
-        setStartDate(start)
-        setDepartment("Engineering")
-        setStreet("34th Peace St")
-        setCity("Chicago")
-        setUsState("Illinois")
-        setZipCode(60018)
+    function fillTheForm(id){
+        if(id === 1){
+            const birth = new Date()
+            birth.setFullYear(1983)
+            birth.setMonth(6)
+            birth.setDate(27)
+            const start = new Date()
+            start.setFullYear(2017)
+            start.setMonth(11)
+            start.setDate(1)
+            setFirstName("John")
+            setLastName("Smith")
+            setDateOfBirth(birth)
+            setStartDate(start)
+            setDepartment("Engineering")
+            setStreet("34th Peace St")
+            setCity("Chicago")
+            setUsState(getRandomValue(statesNames))
+            setZipCode(60018)
+        } else {
+            const birth = new Date()
+            birth.setFullYear(1992)
+            birth.setMonth(8)
+            birth.setDate(30)
+            const start = new Date()
+            start.setFullYear(2020)
+            start.setMonth(3)
+            start.setDate(9)
+            setFirstName("Elsa")
+            setLastName("Young")
+            setDateOfBirth(birth)
+            setStartDate(start)
+            setDepartment("Marketing")
+            setStreet("3rd East St")
+            setCity("Detroit")
+            setUsState("Michigan")
+            setZipCode(48201)
+        }
     }
 
     return(
         <main>
             <section className="content_width">
                 <h1 className="section-title"><span>Create</span> an employee.</h1>
-                <FillForm onClick={() => fillTheForm()}>fill form</FillForm>
+                <FillForm onClick={() => fillTheForm(1)}>fill form</FillForm>
                 <form onSubmit={handleLoginSubmit}>
                     <InputsWrapper>
                         <div className="inputs-col">
@@ -134,35 +159,41 @@ function CreateEmployee(){
                                 onChange={setDepartment}
                             />
                         </div>
-                        <div className="inputs-col inputs-address">
-                            <TextInput
-                                label="Street"
-                                placeholder="Your street"
-                                value={street}
-                                onChange={setStreet}
-                            />
-                            <TextInput
-                                label="City"
-                                placeholder="Your city"
-                                value={city}
-                                onChange={setCity}
-                            />
-                            <Select
-                                label="State"
-                                placeholder="Your state"
-                                searchable
-                                clearable
-                                nothingFound="No options"
-                                data={statesNames}
-                                value={usState}
-                                onChange={setUsState}
-                            />
-                            <NumberInput
-                                label="Zip code"
-                                placeholder="Your zip code"
-                                value={zipCode}
-                                onChange={setZipCode}
-                            />
+                        <div className="inputs-col input-col-address">
+                            <InputsAddress>
+                                <h2>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg>
+                                    Address
+                                </h2>
+                                <TextInput
+                                    label="Street"
+                                    placeholder="Your street"
+                                    value={street}
+                                    onChange={setStreet}
+                                />
+                                <TextInput
+                                    label="City"
+                                    placeholder="Your city"
+                                    value={city}
+                                    onChange={setCity}
+                                />
+                                <Select
+                                    label="State"
+                                    placeholder="Your state"
+                                    searchable
+                                    clearable
+                                    nothingFound="No options"
+                                    data={statesNames}
+                                    value={usState}
+                                    onChange={setUsState}
+                                />
+                                <NumberInput
+                                    label="Zip code"
+                                    placeholder="Your zip code"
+                                    value={zipCode}
+                                    onChange={setZipCode}
+                                />
+                            </InputsAddress>
                         </div>
                     </InputsWrapper>
 
@@ -178,7 +209,7 @@ export default CreateEmployee
 
 const FillForm = styled.span`
     position:absolute;
-    top:0;
+    top:10px;
     right:0;
     font-size:12px;
     line-height:1;
@@ -200,10 +231,23 @@ const InputsWrapper = styled.div`
     justify-content:space-between;
     & .inputs-col{
         width:calc(50% - 20px);
-        &.inputs-address{
-            background-color:${colors.light1};
-            padding:20px;
-            border-radius:4px;
+    }
+    & .input-col-address{
+        padding-top:22px;
+    }
+`
+const InputsAddress = styled.div`
+    background-color:${colors.light1};
+    padding:20px;
+    border-radius:4px;
+    & h2{
+        font-size:20px;
+        font-weight:900;
+        margin-bottom:10px;
+        & svg{
+            height:17px;
+            margin-right:5px;
+            fill:${colors.primary};
         }
     }
 `
