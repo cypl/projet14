9cbdd504c4adbb592869ea194994487fbf59e51a
cypl <cyrille.plenchette@me.com>
2023-06-29 21:35:47 +0200

    add theme styles Mantine

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/index.css'
+import { MantineProvider } from '@mantine/core'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+        <MantineProvider
+          theme={{
+            // Override any other properties from default theme
+            fontFamily: 'sans-serif',
+            fontWeight: '400',
+            spacing: { xs: '1rem', sm: '1.2rem', md: '1.8rem', lg: '2.2rem', xl: '2.8rem' },
+          }}
+        >
+      <App />
+    </MantineProvider>
   </React.StrictMode>,
 )
