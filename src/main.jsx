923caa95fe00762645fbe993018bcb409e7da3dc
cypl <cyrille.plenchette@me.com>
2023-06-30 17:01:55 +0200

    fix redux react dependency

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import { MantineProvider } from '@mantine/core'
 import { store } from './store/store'
 import { Provider } from 'react-redux'
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
