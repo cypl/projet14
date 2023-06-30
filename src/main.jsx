a21789d07e33fb1d1a6b8007042bef8c9f307dfe
cypl <cyrille.plenchette@me.com>
2023-06-30 16:26:03 +0200

    add Mantine styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/index.css'
 import { MantineProvider } from '@mantine/core'
+//import { colors } from './utils/colors.js'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
         <MantineProvider
           theme={{
-            // Override any other properties from default theme
-            fontFamily: 'sans-serif',
+            colorScheme: 'light',
+            colors: {
+              brand: ['#a1a7ff', '#9094ff', '#7f82ff', '#6f6fff', '#5e5cff', '#4d4aff', '#3c37ff', '#322cd7', '#2720af', '#1d1587'],
+            },
             fontWeight: '400',
             spacing: { xs: '1rem', sm: '1.2rem', md: '1.8rem', lg: '2.2rem', xl: '2.8rem' },
+            primaryColor: 'brand',
+            defaultRadius: 'sm',
           }}
         >
       <App />
