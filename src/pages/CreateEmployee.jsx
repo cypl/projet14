b0088edf9b725c782f80ef71a144ace9710c1fa8
cypl <cyrille.plenchette@me.com>
2023-07-13 10:42:27 +0200

    change Icon Addres

--- src/pages/CreateEmployee.jsx
+++ src/pages/CreateEmployee.jsx
@@ -5,7 +5,7 @@ import { DatePickerInput } from "@mantine/dates"
 import { useDisclosure } from '@mantine/hooks'
 import styled from 'styled-components'
 import { colors } from "../utils/colors"
-import { IconCalendar, IconMagic } from "../utils/Icons"
+import { IconCalendar, IconMagic, IconAdress } from "../utils/Icons"
 import { statesNames } from "../utils/statesList"
 import { departmentsList } from "../utils/departmentsList"
 import { getRandomValue, randomFirstNames, randomLastNames, randomBirthYears, randomStartYears, randomMonths, randomDays, randomStreets, randomCities, randomStates, randomZipCodes } from "../utils/randomData"
@@ -40,9 +40,11 @@ const InputsAddress = styled.div`
         font-weight:900;
         margin-bottom:10px;
         & svg{
-            height:17px;
+            height:20px;
+            width:20px;
             margin-right:5px;
-            fill:${colors.primary};
+            vertical-align:-2px;
+            color:${colors.primary};
         }
     }
 `
@@ -284,7 +286,7 @@ function CreateEmployee(){
                         <div className="inputs-col input-col-address">
                             <InputsAddress>
                                 <h2>
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg>
+                                    <IconAdress/>
                                     Address
                                 </h2>
                                 <TextInput
