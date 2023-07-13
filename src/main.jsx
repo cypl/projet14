89bd4dd2a2572628053d957653d776d403687ad0
cypl <cyrille.plenchette@me.com>
2023-07-13 10:15:19 +0200

    add global CSS Mantine UI

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './styles/index.css'
 import { MantineProvider } from '@mantine/core'
 import { store } from './store/store'
 import { Provider } from 'react-redux'
+import { colors } from './utils/colors.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -15,10 +16,32 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             colors: {
               brand: ['#a1a7ff', '#9094ff', '#7f82ff', '#6f6fff', '#5e5cff', '#4d4aff', '#3c37ff', '#322cd7', '#2720af', '#1d1587'],
             },
-            fontWeight: '400',
             spacing: { xs: '1rem', sm: '1.2rem', md: '1.8rem', lg: '2.2rem', xl: '2.8rem' },
             primaryColor: 'brand',
             defaultRadius: 'sm',
+            black:`${colors.secondary2}`,
+            globalStyles: () => ({
+              '.mantine-InputWrapper-root':{
+                paddingBottom:"20px",
+                position: "relative",
+              },
+              '.mantine-Input-wrapper, .mantine-TextInput-wrapper':{
+                marginBottom:0,
+              },
+              '.mantine-InputWrapper-required, .mantine-TextInput-required':{
+                color:`${colors.primary}`,
+              },
+              '.mantine-InputWrapper-label, .mantine-TextInput-label': {
+                  fontWeight:400,
+              },
+              '.mantine-InputWrapper-error':{
+                position: "absolute",
+                bottom:0,
+              },
+              '.mantine-Button-leftIcon':{
+                marginRight:"0.5rem",
+              }
+            }),
           }}
         >
         <App />
