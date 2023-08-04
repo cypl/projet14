daccfa9f227aca371f16647b3a223cf741ea1bc7
cypl <cyrille.plenchette@me.com>
2023-08-04 14:32:20 +0200

    -

--- src/main.jsx
+++ src/main.jsx
@@ -12,20 +12,20 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
         <MantineProvider
-          theme={{
-            colorScheme: 'light',
-            colors: {
-              brand: ['#a1a7ff', '#9094ff', '#7f82ff', '#6f6fff', '#5e5cff', '#4d4aff', '#3c37ff', '#322cd7', '#2720af', '#1d1587'],
-            },
-            spacing: { xs: '1rem', sm: '1.2rem', md: '1.8rem', lg: '2.2rem', xl: '2.8rem' },
-            primaryColor: 'brand',
-            defaultRadius: 'sm',
-            black:`${colors.secondary2}`,
-            globalStyles: () => HRStyles(),
-          }}
-        >
-        <App />
-      </MantineProvider>
+            theme={{
+              colorScheme: 'light',
+              colors: {
+                brand: ['#a1a7ff', '#9094ff', '#7f82ff', '#6f6fff', '#5e5cff', '#4d4aff', '#3c37ff', '#322cd7', '#2720af', '#1d1587'],
+              },
+              spacing: { xs: '1rem', sm: '1.2rem', md: '1.8rem', lg: '2.2rem', xl: '2.8rem' },
+              primaryColor: 'brand',
+              defaultRadius: 'sm',
+              black:`${colors.secondary2}`,
+              globalStyles: () => HRStyles(),
+            }}
+          >
+          <App />
+        </MantineProvider>
     </Provider>
   </React.StrictMode>,
 )
