8b3e0c2677530684e44c86a59045ca2aa5165a9b
cypl <cyrille.plenchette@me.com>
2023-08-04 16:40:32 +0200

    complete error page 404

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,9 +1,31 @@
+import { NavLink } from "react-router-dom"
+import styled from 'styled-components'
+import { Button } from "@mantine/core"
+
+const SectionError = styled.section`
+    & .section-title{
+        text-align:center;
+    }
+    & p{
+        text-align:center;
+    }
+`
+const BtnWrapper = styled.div`
+    padding-top:20px;
+    display:flex;
+    justify-content:center;
+`
+
 function Error(){
     return(
         <main>
-            <section className="content_width">
-                <p>Here, you can see an error.</p>
-            </section>
+            <SectionError className="content_width">
+                <h1 className="section-title">Error <span>404</span></h1>
+                <p>Sorry, this page doesn’t exist.</p>
+                <BtnWrapper>
+                    <Button><NavLink to="/">Go to homepage</NavLink></Button>
+                </BtnWrapper>
+            </SectionError>
         </main>
     )
 }
