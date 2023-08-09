992eb35550a42fe49c8e18509576b0e8d32d5d5a
cypl <cyrille.plenchette@me.com>
2023-08-09 15:07:07 +0200

    add JS Doc

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -1,7 +1,13 @@
 import { useState, useEffect } from "react"
 import axios from 'axios'
 
-
+/**
+ * Use this hook to make a GET request on a predefined path:
+ * get the data, the loading status and the potential errors.
+ * 
+ * @param {string} path Path to the resource to retrieve.
+ * @returns {{data: Object, isLoaded: boolean, isError: Object}} Returns an object containing the retrieved data, a boolean indicating whether the loading is complete, and any error.
+ */
 const useFetchData = (path) => {
     const [data, setData] = useState({})
     const [isLoaded, setLoaded] = useState(false)
@@ -27,5 +33,11 @@ const useFetchData = (path) => {
 
 }
 
-
-export const GetDataEmployees = () => { return useFetchData("/data/mocked-employees.json") }
+/**
+ * Retrieves employee data from a predefined path.
+ * 
+ * @returns {{data: Object, isLoaded: boolean, isError: Object}} Returns an object containing the retrieved data, a boolean indicating whether the loading is complete, and any error.
+ */
+export const GetDataEmployees = () => { 
+    return useFetchData("/data/mocked-employees.json") 
+}
