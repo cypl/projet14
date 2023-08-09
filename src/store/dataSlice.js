45fb70e6365d805e028429dd5f75d2b55abbfb63
cypl <cyrille.plenchette@me.com>
2023-08-09 15:28:57 +0200

    add store related JS Doc

--- src/store/dataSlice.js
+++ src/store/dataSlice.js
@@ -1,18 +1,42 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+/**
+ * The initial state of the `employees` slice. 
+ * It represents an array containing employees data.
+ * 
+ * @type {{ data: Object[] }}
+ */
 const initialState = {
     data: [] 
 }
 
+/**
+ * A slice for managing the employees data in the Redux store.
+ * 
+ * This slice contains reducers to set the initial employees data 
+ * and to add a new employee to the list.
+ */
 export const dataSlice = createSlice({
   name: 'employees',
   initialState,
   reducers: {
+    /**
+     * Set the initial employees data.
+     * 
+     * @param {Object} state The current state object.
+     * @param {Object} action The dispatched action containing the payload with the initial data.
+     */
     setInitialData: (state, action) => {
       state.data = action.payload
     },
+    /**
+     * Add a new employee to the list of employees.
+     * 
+     * @param {Object} state The current state object.
+     * @param {Object} action The dispatched action containing the payload with the new employee data.
+     */
     addEmployee: (state, action) => {
-        state.data.push(action.payload)
+      state.data.push(action.payload)
     },
   },
 })
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit"
 import dataSlice from "./dataSlice"
 
+/**
+ * The Redux store of the application.
+ * 
+ * Currently, the store consists of a single slice `employees` which is
+ * managed by the `dataSlice`.
+ */
 export const store = configureStore({
   reducer: {
     employees: dataSlice,
