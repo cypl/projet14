ee2cfaebc0ac14fa7c166c31c70d67092b295729
cypl <cyrille.plenchette@me.com>
2023-08-09 21:49:54 +0200

    add SuccessModalContent component

---
+++ src/layouts/SuccessModalContent.jsx
@@ -0,0 +1,45 @@
+import { useContext } from "react"
+import { useSelector } from "react-redux"
+import { NavLink } from "react-router-dom"
+import { Button } from '@mantine/core'
+import styled from 'styled-components'
+import { ModalContext } from "react-modal-classic"
+import { colors } from '../utils/colors'
+
+const ContentModal = styled.div`
+    padding:30px;
+    text-align:center;
+    & .modal-content-newemployee{
+        padding-bottom:20px;
+        & strong{
+            font-weight:900;
+            color:${colors.primary};
+        }
+    }
+    & .modal-content-button{
+        margin:0 10px;
+    }
+`
+
+/**
+ * Displays the content of the modal, after successfull form submit (see CreateEmployee page component).
+ * @returns {JSX.Element} - The JSX markup for the SuccessModalContent component.
+ */
+function SuccessModalContent(){
+    
+    const currentEmployees = useSelector((state) => state.employees.data)
+    const reverseCurrentEmployees = [...currentEmployees].reverse()
+    
+    const { closeModal } = useContext(ModalContext)
+
+    return (
+        <ContentModal>
+            <p className="modal-content-newemployee">New employee added:<br/>
+            <strong>{reverseCurrentEmployees[0].firstName} {reverseCurrentEmployees[0].lastName}</strong></p>
+            <Button variant={"outline"} onClick={closeModal} className="modal-content-button">Add a new one</Button>
+            <NavLink to="/"><Button className="modal-content-button">Check the list</Button></NavLink>
+        </ContentModal>
+    )
+}
+
+export default SuccessModalContent
