9b6dc490b011059d9650766764891da2563f3469
cypl <cyrille.plenchette@me.com>
2023-08-09 15:17:37 +0200

    add JS Doc

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,8 +1,19 @@
 import styled from 'styled-components'
 import { colors } from '../utils/colors'
 
+const FooterContainer = styled.footer`
+    border-top:1px solid ${colors.light2};
+`
+const FooterContent = styled.div`
+    padding:40px 0;
+`
+
 const currentYear = new Date().getFullYear()
 
+/**
+ * Displays the footer of the application.
+ * @returns {JSX.Element} - The JSX markup for the Footer component.
+ */
 function Footer(){
     return(
         <FooterContainer>
@@ -14,10 +25,3 @@ function Footer(){
 }
 
 export default Footer
-
-const FooterContainer = styled.footer`
-    border-top:1px solid ${colors.light2};
-`
-const FooterContent = styled.div`
-    padding:40px 0;
-`
