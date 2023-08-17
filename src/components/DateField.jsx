648d553e4e1eb52237e8262099423e4c4285c6f9
cypl <cyrille.plenchette@me.com>
2023-08-17 11:30:40 +0200

    match datepicker styles with app styles

--- src/components/DateField.jsx
+++ src/components/DateField.jsx
@@ -14,6 +14,41 @@ const InputFieldWrapper = styled.div`
     & .react-datepicker__triangle{
         display:none;
     }
+    & .react-datepicker{
+        border:1px solid ${colors.light2};
+        font-size:0.75rem;
+    }
+    & .react-datepicker__header{
+        background-color:${colors.light1};
+        border-bottom:1px solid ${colors.light2};
+    }
+    & .react-datepicker__current-month{color:${colors.secondary2};font-size:0.875rem;font-weight:500;}
+    & .react-datepicker__header{color:${colors.secondary2};}
+    & .react-datepicker__day-name{color:${colors.secondary2};}
+    & .react-datepicker__year-read-view--down-arrow{margin-top:2px;}
+    & .react-datepicker__day{
+        color:${colors.primary};
+        &:hover{
+            background-color:${colors.light1};
+        }
+        &.react-datepicker__day--disabled{
+            color:${colors.light2};
+            &:hover{
+                background-color:transparent;
+            }
+        }
+        &.react-datepicker__day--selected{
+            background-color:${colors.primary};
+            color:#fff;
+        }
+        &.react-datepicker__day--keyboard-selected{
+            background-color:${colors.primary1};
+        }
+        &.react-datepicker__day--today{
+            font-weight:900;
+        }
+    }
+
 `
 const InputField = styled(DatePicker)`
     width:100%;
